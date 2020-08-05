45831b0c87790139c389dd0838ae8b05e42fa61e
Jim Tyhurst <jim@tyhurst.com>
2020-08-05 15:30:44 -0700

    Upgraded plotnine.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 
 # Builds a docker image named 'more-tensorflow-notebook',
 # using the 'Dockerfile' in the current directory.
-docker build --rm -t more-tensorflow-notebook .
+docker build --rm -t jimtyhurst/more-tensorflow-notebook:latest .
+
